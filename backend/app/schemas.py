557f8a05ec40f6557264da2ae439dcdfca112358
Philip Widoff <philip.widoff@gmail.com>
2024-03-06 08:30:08 +0100

    merge

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -35,9 +35,9 @@ class TravelUserSchema(BaseModel):
 class ExperienceSchema(BaseModel):
     title: str = Field(..., min_length=1, max_length=100,
                             description="The name of the experience.")
-    description: str = Field(...,min_length=3, max_length=25
+    description: str = Field(...,min_length=3, max_length=255,
                                 description="The description of the experience.",
-                                       5)
+                                       )
     country_id: int = Field(..., description="id for which country it is in")
     latitude: float
     longitude: float
