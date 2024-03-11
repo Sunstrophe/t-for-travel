7e5e9c1263f4a3585bc20bcf7c0f242ba3d517e9
Sunstrophe <mackansund_2@live.se>
2024-03-11 18:47:11 +0100

    added verification and limitation

--- backend/app/prompting.py
+++ backend/app/prompting.py
@@ -5,25 +5,56 @@ from dotenv import load_dotenv
 load_dotenv()
 
 client = OpenAI(api_key=getenv("OPENAI_API_KEY"))
+max_length = 100
 
 
-def call_ai():
+def handle_call(input):
+    if count_input_token(input) > max_length:
+        raise Exception
+    response = call_ai(input)
+    content = response.choices[0].message.content
+    # log_output(response)
+    if not verify_output(content):
+        raise Exception
+    print(content)
+    return content
+
+# Is supposed to approximate the amount of tokens used by user input
+# Currently it only counts length
+
+
+def count_input_token(input):
+    return len(input)
+
+
+def call_ai(input):
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         response_format={"type": "json_object"},
         messages=[
             {"role": "system", "content": "You are a helpful assistant designed to output JSON composed of latitude and longitude keys."},
-            {"role": "user", "content": "music"}
+            {"role": "user", "content": input}
         ]
     )
     # Returns the following format
     # ChatCompletionMessage(content='{\n    "latitude": 0.0,\n    "longitude": 0.0\n}', role='assistant', function_call=None, tool_calls=None)
-    print(response.choices[0].message)
 
     # Prints content of response message
     # print(response.choices[0].message.content)
+    return response
 
-    # print(response.usage)
+
+# Log output either to file on server or db
+def log_output():
+    ...
 
 
-call_ai()
+# Make sure out put has atleast latitude and longitude keys
+def verify_output(message_data):
+    if "latitude" and "longitude" in message_data:
+        return True
+    return False
+
+
+if __name__ == "__main__":
+    handle_call("Beach, europe")
