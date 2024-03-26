2c79edcb23f441babd182ea5487b88670987e120
Sunstrophe <mackansund_2@live.se>
2024-03-26 21:05:31 +0100

    Working on cleaning up the userpage

--- frontend/src/pages/UserMyPosts.jsx
+++ frontend/src/pages/UserMyPosts.jsx
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import placeholderImage from "../assets/placeholder_image.jpg"
 
 function UserMyPosts() {
+    const [userPosts, setUserPosts] = useState([]);
 
+    useEffect(() => {
+        getUserPosts();
+    }, []);
 
-
-    const getUserPosts = async() => {
+    const getUserPosts = async () => {
         // Url for user will be implemented in the future
-        url = "http://"
-        response = await fetch(url)
-    }
-
+        const user_id = "1";
+        const url = `http://localhost:8000/user/${user_id}/experience`;
+        const response = await fetch(url);
+        const data = await response.json();
+        // console.log(data[0].title)
+        setUserPosts(data);
+    };
 
     return (
         <div className="max-h-full px-8 py-6 overflow-y-auto">
-            <p>UserMyPosts</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa reiciendis possimus enim doloribus non dicta neque aliquam labore fugit earum tempora amet repellendus debitis ab sed, magni deserunt eius aspernatur temporibus, hic, voluptate repellat asperiores. Eos ratione nihil totam officiis, nam sint incidunt deleniti, dolorem, facere animi ea molestias nemo error laudantium? Eaque nostrum architecto nam incidunt, eveniet error id harum exercitationem sint, tempore, maxime distinctio. Autem, sed distinctio quibusdam aliquam tempore facere labore voluptates recusandae, accusantium illum laboriosam magnam officia perferendis quas ea, laborum animi. Deleniti natus, quo cumque fuga hic obcaecati veritatis ea, laudantium, quae velit id repellat!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa reiciendis possimus enim doloribus non dicta neque aliquam labore fugit earum tempora amet repellendus debitis ab sed, magni deserunt eius aspernatur temporibus, hic, voluptate repellat asperiores. Eos ratione nihil totam officiis, nam sint incidunt deleniti, dolorem, facere animi ea molestias nemo error laudantium? Eaque nostrum architecto nam incidunt, eveniet error id harum exercitationem sint, tempore, maxime distinctio. Autem, sed distinctio quibusdam aliquam tempore facere labore voluptates recusandae, accusantium illum laboriosam magnam officia perferendis quas ea, laborum animi. Deleniti natus, quo cumque fuga hic obcaecati veritatis ea, laudantium, quae velit id repellat!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa reiciendis possimus enim doloribus non dicta neque aliquam labore fugit earum tempora amet repellendus debitis ab sed, magni deserunt eius aspernatur temporibus, hic, voluptate repellat asperiores. Eos ratione nihil totam officiis, nam sint incidunt deleniti, dolorem, facere animi ea molestias nemo error laudantium? Eaque nostrum architecto nam incidunt, eveniet error id harum exercitationem sint, tempore, maxime distinctio. Autem, sed distinctio quibusdam aliquam tempore facere labore voluptates recusandae, accusantium illum laboriosam magnam officia perferendis quas ea, laborum animi. Deleniti natus, quo cumque fuga hic obcaecati veritatis ea, laudantium, quae velit id repellat!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa reiciendis possimus enim doloribus non dicta neque aliquam labore fugit earum tempora amet repellendus debitis ab sed, magni deserunt eius aspernatur temporibus, hic, voluptate repellat asperiores. Eos ratione nihil totam officiis, nam sint incidunt deleniti, dolorem, facere animi ea molestias nemo error laudantium? Eaque nostrum architecto nam incidunt, eveniet error id harum exercitationem sint, tempore, maxime distinctio. Autem, sed distinctio quibusdam aliquam tempore facere labore voluptates recusandae, accusantium illum laboriosam magnam officia perferendis quas ea, laborum animi. Deleniti natus, quo cumque fuga hic obcaecati veritatis ea, laudantium, quae velit id repellat!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa reiciendis possimus enim doloribus non dicta neque aliquam labore fugit earum tempora amet repellendus debitis ab sed, magni deserunt eius aspernatur temporibus, hic, voluptate repellat asperiores. Eos ratione nihil totam officiis, nam sint incidunt deleniti, dolorem, facere animi ea molestias nemo error laudantium? Eaque nostrum architecto nam incidunt, eveniet error id harum exercitationem sint, tempore, maxime distinctio. Autem, sed distinctio quibusdam aliquam tempore facere labore voluptates recusandae, accusantium illum laboriosam magnam officia perferendis quas ea, laborum animi. Deleniti natus, quo cumque fuga hic obcaecati veritatis ea, laudantium, quae velit id repellat!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa reiciendis possimus enim doloribus non dicta neque aliquam labore fugit earum tempora amet repellendus debitis ab sed, magni deserunt eius aspernatur temporibus, hic, voluptate repellat asperiores. Eos ratione nihil totam officiis, nam sint incidunt deleniti, dolorem, facere animi ea molestias nemo error laudantium? Eaque nostrum architecto nam incidunt, eveniet error id harum exercitationem sint, tempore, maxime distinctio. Autem, sed distinctio quibusdam aliquam tempore facere labore voluptates recusandae, accusantium illum laboriosam magnam officia perferendis quas ea, laborum animi. Deleniti natus, quo cumque fuga hic obcaecati veritatis ea, laudantium, quae velit id repellat!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa reiciendis possimus enim doloribus non dicta neque aliquam labore fugit earum tempora amet repellendus debitis ab sed, magni deserunt eius aspernatur temporibus, hic, voluptate repellat asperiores. Eos ratione nihil totam officiis, nam sint incidunt deleniti, dolorem, facere animi ea molestias nemo error laudantium? Eaque nostrum architecto nam incidunt, eveniet error id harum exercitationem sint, tempore, maxime distinctio. Autem, sed distinctio quibusdam aliquam tempore facere labore voluptates recusandae, accusantium illum laboriosam magnam officia perferendis quas ea, laborum animi. Deleniti natus, quo cumque fuga hic obcaecati veritatis ea, laudantium, quae velit id repellat!</p>
+            <div className="flex flex-col gap-4">
+                {userPosts.length === 0 ? (
+                    <p>Nothing posted!</p>
+                ) : (
+                    userPosts.map((experience, index) => {
+                        return (
+                            <div className="grid grid-cols-3 border-2 border-black h-60" key={index}>
+                                <div className="flex flex-col col-span-2 gap-2 p-4">
+                                    <h4 className="text-lg font-bold text-center">{experience.title}</h4>
+                                    <p className="text-sm">{experience.description}</p>
+                                </div>
+                                <div className="flex items-center justify-center col-span-1">
+                                    <img className="w-auto h-auto max-w-full max-h-full" src={placeholderImage} alt="Placeholder" />
+                                </div>
+                            </div>
+                        );
+                    })
+                )}
+            </div>
         </div>
     );
 }
