fdb3015b3e99c1f05ae4131b8abe2b1279e652c1
Sunstrophe <mackansund_2@live.se>
2024-03-26 11:46:08 +0100

    Now fetches all markers with a 2 unit of latlng square around the center of the map

--- frontend/src/components/CloseExperiences.jsx
+++ frontend/src/components/CloseExperiences.jsx
@@ -1,44 +1,46 @@
-import React, { useState } from 'react'
-import { Marker, useMapEvent } from 'react-leaflet'
+import React, { useEffect, useState } from "react";
+import { Marker, useMapEvent } from "react-leaflet";
 
 function CloseExperiences() {
-    const [experiences, setExeperiences] = useState(null)
+    const [experiences, setExeperiences] = useState(null);
 
-    const getExperiences = async() => {
-        const mapCenter = map.getCenter()
-        const lat = mapCenter["lat"]
-        const lng = mapCenter["lng"]
-    
+    const getExperiences = async () => {
+        const mapCenter = map.getCenter();
+        const lat = mapCenter["lat"];
+        const lng = mapCenter["lng"];
+
         const response = await fetch(`http://localhost:8000/experience?lat=${lat}&lng=${lng}`, {
-          method: "GET",
-        })
+            method: "GET",
+        });
         if (!response.ok) {
-          throw new Error("Error getting experiences")
+            throw new Error("Error getting experiences");
         }
-        const data = await response.json()
-        console.log(data)
-        setExeperiences(data)
-      }
+        const data = await response.json();
+        console.log(data);
+        setExeperiences(data);
+    };
 
     const map = useMapEvent({
-        moveend () {
+        moveend() {
             // console.log("TEST!!!")
-            getExperiences()
-        }
-    })
+            getExperiences();
+        },
+    });
 
-    const getMarker = (experience) => {
-        console.log(experience)
-        const latitude = experience["latitude"]
-        const longitude = experience["longitude"]
-        const position = latitude + ", " + longitude
-        return <Marker position={position} />
-    }
+    useEffect(() => {
+        getExperiences();
+    }, []);
 
+    const renderMarker = () => {
+        if (!experiences) return null;
 
-  return experiences === null ? null : 
-    (experiences.forEach(getMarker))
-  
+        return experiences.map((experience) => {
+            const { latitude, longitude } = experience;
+            return <Marker position={[latitude, longitude]} key={experience.id} />;
+        });
+    };
+
+    return renderMarker();
 }
 
-export default CloseExperiences
+export default CloseExperiences;
